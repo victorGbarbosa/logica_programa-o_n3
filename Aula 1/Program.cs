718ddeffa204a8d8444cfe3ef6d1da2979775285
VICTOR GABRIEL BARBOSA <victor.gbarbosa1@senacsp.edu.br>
2025-11-03 20:42:20 -0300

    primeiro commit com codigo

---
+++ Aula 1/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+// f5 EXECUTAR EM MODODEBUG 
+// CTRL 
+
+// declaração de uma variável 
+string nome;
+
+//atribuição de valor a uma variável 
+nome= "victor gabriel";
+
+Console.WriteLine(nome);
+
+nome = "joão";
+
+Console.WriteLine(nome);
+
+//declaração + atribuição 
+const string constante = "minha constante";
+
+
+Console.WriteLine(constante);
+
+//tipo, nome,=,valor,;
+
+int idade = 27;
+long inteirogrande = 0;
+float alturafloat = 1.9f;
+double altura = 1.9;
+char letra = 'A';
+
+bool booleano = true;
+bool booleano = false;
