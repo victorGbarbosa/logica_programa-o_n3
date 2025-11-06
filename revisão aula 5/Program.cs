637086caec7e518f0e10aac6055b16152b642ba0
VICTOR GABRIEL BARBOSA <victor.gbarbosa1@senacsp.edu.br>
2025-11-05 21:58:38 -0300

    aula 5

---
+++ revisão aula 5/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int n1;
+int n2;
+
+//escrevi uma mensagem no console
+Console.WriteLine("insira um numero inteiro :");
+//li o conteudo de uma linha, quando o usuario apertou a tecla ENTER 
+//o console só aceita texto (string)
+//logo é preciso converter a string para (int)
+n1 = int.Parse(Console.ReadLine());
+
+//escrevi uma mensagem no console
+Console.WriteLine("insira mais um numero inteiro: ");
+//novamente li o conteudo da linha, quando o usuario apertou a tecla ENTER 
+n2 = int.Parse(Console.ReadLine());
+
+//Mostrar os dois numeros no console 
+
+Console.WriteLine("voce digitou " + n1 + " e " + n2);
+
+int soma = n1 + n2;
+int sub = n1 - n2;
+
+Console.WriteLine("a soma eh: " + soma);
+Console.WriteLine("a subtração eh" + sub);
+
+
